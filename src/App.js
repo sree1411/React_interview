6affc58dfb5480b08ca40fc7f727ebf503c9d62d
sreekanth <sreekanth>
2024-12-22 17:15:14 +0530

    crud operation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,65 @@
 
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+
+  const [newUser, setNewUser]= useState('')
+  const [users, setUsers] = useState([]);
+  const [editIndex, setEditIndex] = useState(null)
+
+   const handleChange = (e)=>{
+      setNewUser(e.target.value)
+   }
+
+
+  const addDetails = ()=>{
+    if(editIndex === null){
+      setUsers([...users, newUser])
+    }else{
+      const updatedUser= users.map((user,index)=>
+      index === editIndex ? newUser : user
+      )
+      setUsers(updatedUser)
+      setEditIndex(null)
+    }
+     
+     setNewUser('')
+  }
+
+
+  const deleteBtn = (i)=>{
+     var temp=[...users]
+     temp.splice(i,1)
+     setUsers(temp)
+  }
+
+  const editBtn = (i)=>{
+    var temp=[...users]
+    setNewUser(temp[i])
+    setEditIndex(i)
+  }
+
   return (
       <div>
-          <h1> welcome to the application </h1>
+          
+          
+           <input type='text' value={newUser}  onChange={handleChange}  placeholder='Enter the text' />
+           <button type='text' onClick={addDetails} > {editIndex=== null ? "add" :"update"} </button>
+
+            <ul>
+               {
+                users.map((person,i)=>(
+                  <li key={i}>
+                    {person}
+                    <button onClick={()=>deleteBtn(i)}> Delete </button>
+                    <button onClick={()=>editBtn(i)}> Edit </button>
+                  </li>
+                ))
+               }
+            </ul>
+
       </div>
   );
 }
