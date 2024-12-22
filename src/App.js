bad486c3fab6f6b7ec7bd8f843ec36b700ca16ee
sreekanth <sreekanth>
2024-12-22 09:52:59 +0530

    api calling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [products, setProducts] = useState([])
+     
+  useEffect(()=>{
+    
+  const fetchData = async ()=>{
+      let response = await fetch("https://fakestoreapi.com/products")
+      let data = await response.json()
+      setProducts(data)
+  }
+  fetchData()
+  },[])
+
+
+
   return (
       <div>
           <h1> welcome to the application </h1>
+          <ul>
+              {
+                products.map((product)=>(
+                  <li>
+                    {product.title}
+                  </li>
+                ))
+              }
+          </ul>
       </div>
   );
 }
